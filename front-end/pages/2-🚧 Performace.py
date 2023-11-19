32766c3dd6cd48d97a3f0224ebb29f3676940331
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-11-19 07:08:32 -0800

    Update 2-🚧 Performace.py

--- front-end/pages/2-🚧 Performace.py
+++ front-end/pages/2-🚧 Performace.py
@@ -1,31 +1,33 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Performance Measures",layout="wide")
+st.set_page_config(page_title="Performance Measures", layout="wide")
 st.title('Performance Measures 🚧')
 st.image('https://miro.medium.com/v2/resize:fit:1400/1*g9x71-cqZ5im3EaFxwyf9Q.jpeg')
 # load the performance metrics df
 
-performance_df = pd.read_csv("/Volumes/Project-2/programming/machine_deep_learning/DL-project/front-end/assets/csv/performance_metrics.csv")
+performance_df = pd.read_csv("./assets/csv/performance_metrics.csv")
 
 # 1. Precision, Recall, and F1 Score of the Machine Learning Models
 
-st.subheader("1. Precision, Recall, and F1 Score of the Machine Learning Models")
+st.subheader(
+    "1. Precision, Recall, and F1 Score of the Machine Learning Models")
 
 # Display the data-frame
 st.dataframe(performance_df)
 
-# plot the performance 
+# plot the performance
 
 st.write("plot the performance ")
 
-st.bar_chart(performance_df,x='Model',y=['Accuracy','Precision','Recall','F1 Score'])
+st.bar_chart(performance_df, x='Model', y=[
+             'Accuracy', 'Precision', 'Recall', 'F1 Score'])
 
 
 st.markdown("**Note:** The model with the highest F1 score is the best model.")
 
 
-st.success("Based on the evaluation results for 10% of the data, it is evident that MobileNet_v2 outperforms the other models in terms of accuracy, precision, recall, and F1 score. Here's a brief conclusion:.",icon="🔥")
+st.success("Based on the evaluation results for 10% of the data, it is evident that MobileNet_v2 outperforms the other models in terms of accuracy, precision, recall, and F1 score. Here's a brief conclusion:.", icon="🔥")
 
 st.divider()
 # explain the results
@@ -44,9 +46,10 @@ st.write(" MobileNet_v2, a lightweight convolutional neural network, demonstrate
 # plot the performance of  MobileNet_v2
 
 st.write("plot the performance of  MobileNet_v2")
-mobile_net_model=performance_df.loc[performance_df['Model'] == 'MobileNet_v2']
+mobile_net_model = performance_df.loc[performance_df['Model']
+                                      == 'MobileNet_v2']
 st.write(mobile_net_model)
-columns=['Accuracy','Precision','Recall','F1 Score']
+columns = ['Accuracy', 'Precision', 'Recall', 'F1 Score']
 
 
 st.bar_chart(mobile_net_model.iloc[0][columns])
@@ -60,7 +63,7 @@ st.write("Resnet50v2 also performs well, but it may be slightly heavier compared
 
 st.write("plot the performance of  Resnet50v2")
 
-resnet50v2_model=performance_df.loc[performance_df['Model'] == 'Resnet50v2']
+resnet50v2_model = performance_df.loc[performance_df['Model'] == 'Resnet50v2']
 
 st.write(resnet50v2_model)
 
@@ -74,7 +77,8 @@ st.markdown("##### 3. EfficientNet Model:")
 st.write("EfficientNet, known for its scalability, achieves high accuracy with less computational complexity.")
 
 # Plot the performance of EfficientNet
-efficientnet_model = performance_df.loc[performance_df['Model'] == 'EfficientNet']
+efficientnet_model = performance_df.loc[performance_df['Model']
+                                        == 'EfficientNet']
 st.write(efficientnet_model)
 
 # Bar chart for EfficientNet performance
@@ -103,4 +107,3 @@ st.write(non_cnn_model)
 st.bar_chart(non_cnn_model.iloc[0][columns])
 
 st.markdown("**🌐 Conclusion:** Based on these findings, it is recommended to proceed with training MobileNet_v2 on the full-scale dataset, as it consistently outperforms other models.")
-
